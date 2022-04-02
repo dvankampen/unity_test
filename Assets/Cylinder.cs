91b6459396dba5008edb516a411d5fbc3a7f72e3
Dave VanKampen <dave.vankampen@gmail.com>
2022-04-02 14:47:45 -0400

    smooth out rotation a bit

--- Assets/Cylinder.cs
+++ Assets/Cylinder.cs
@@ -5,13 +5,60 @@ using UnityEngine.InputSystem;
 
 public class Cylinder : MonoBehaviour
 {
+    public float RotationAmount = 2f;
+    public int TicksPerSecond = 60;
+    public bool Pause = false;
+    public Transform Target;
+    public float Speed = 1f;
+    private Coroutine LookCoroutine;
+
     // Start is called before the first frame update
-    public void Rotate(InputAction.CallbackContext context) {
-        Debug.Log("Cylinder rotating!!");
+    public void RotateUp(InputAction.CallbackContext context) {
         if (context.performed) {
-            transform.Rotate(0, 5, 0);
+            if (LookCoroutine != null) {
+                StopCoroutine(LookCoroutine);
+            }
+            LookCoroutine = StartCoroutine(PitchUp());
         }
+    }
 
+    public void RotateDown(InputAction.CallbackContext context) {
+        if (context.performed) {
+            if (LookCoroutine != null) {
+                StopCoroutine(LookCoroutine);
+            }
+            LookCoroutine = StartCoroutine(PitchDown());
+        }
+    }
 
+    private IEnumerator PitchUp()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, 2, 0);
+        float time = 0;
+
+        while(time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, time);
+            time += Time.deltaTime * Speed;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator PitchDown()
+    {
+        Quaternion startRotation = transform.rotation;
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, -2, 0);
+        float time = 0;
+
+        while(time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, time);
+            time += Time.deltaTime * Speed;
+
+            yield return null;
+        }
     }
 }
+
